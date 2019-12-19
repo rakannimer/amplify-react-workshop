7b67d46f7b8c082faa12b587f46081a7d92f557e
rakannimer <rakannimer@gmail.com>
2019-12-19 19:36:49 +0200

    added amplify setup to react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+const App = () => {
+  return (
+    <div>
+      <Header as="h1">Hello World!</Header>
+    </div>
+  );
+};
+
+export default App;
